4e6c86d8b80b4062dc0a004d68a68a3c62273f2a
berglindbra <berglindbra28@gmail.com>
2021-06-13 18:31:30 +0000

    flattener working

---
+++ script.js
@@ -0,0 +1,8 @@
+
+$(document).ready(function(){
+      $('.comic_seq_2-container').click(function(){
+          $('.flattener-push').addClass('flattener-animation');
+
+          $('svg animate.anim1').attr("begin", 2);
+      });
+});
